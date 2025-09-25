43adb8d8058634e84cf0c98c75d610031b117d8f
wugkqd <wugkqd@gmail.com>
2025-09-25 16:32:45 +0900

    💡State hoisting

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,33 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
   return (
     <div>
-      <Counter/>
-      <Counter/>
+      <Counter
+        count={count1}
+        onIncrement={
+          () => setCount1(prev => prev + 1)
+        }/>
+      <Counter
+        count={count2}
+        onIncrement={
+          () => setCount2(prev => prev + 1)
+       }/>
+      {/* <Counter/> */}
     </div>
   )
 }
 
-function Counter() {
-  const [count, setCount] = useState(0)
+function Counter({count, onIncrement}) {
 
   return (
     <div>
       <h1>Counter: {count}</h1>
       <button 
-        onClick={ () => setCount( prev => prev + 1 ) }>
+        onClick={onIncrement}>
           증가
       </button>
     </div>
